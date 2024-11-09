7d09dbe67dc2653c8545ab7cb893e9427192fded
volvo <shai.v@sygnia.co>
2024-11-09 19:35:08 +0200

    add comment and move scripts

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -2,6 +2,7 @@ from .csv_database import CSVDatabase
 from app.constants import DATA_FILE_PATH
 
 def get_ip2country_db(db_type: str):
+    # DB type should inherit from BaseDatabase and implement the lookup method
     if db_type == "csv":
         return CSVDatabase()
     else:

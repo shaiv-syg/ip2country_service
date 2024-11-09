a5cf11521ea368f3ba47c6e6703b3d8052f115c5
volvo <shai.v@sygnia.co>
2024-11-09 15:58:35 +0200

    run dev

---
+++ run_dev.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate ip2country_env
+
+# Set environment variables
+export REDIS_HOST=localhost
+export REDIS_PORT=6379
+export RATE_LIMIT=2
+
+
+# Run the application
+uvicorn app.main:app --reload 
